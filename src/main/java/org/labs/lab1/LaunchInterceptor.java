972b95c504477766469966b4d1ef5dee410671b2
estolpe <estolpe@kth.se>
2023-01-27 16:35:52 +0100

    Add skeleton structure for LaunchInterceptor

--- src/main/java/org/labs/lab1/LaunchInterceptor.java
+++ src/main/java/org/labs/lab1/LaunchInterceptor.java
@@ -1,4 +1,29 @@
 package org.labs.lab1;
 
 public class LaunchInterceptor {
+
+
+    /**
+     * Decides whether an interceptor should be launched based on
+     * input radar tracking information. Prints YES if launch, NO if no launch.
+     * @param data all input data: lcm, puv, points, parameters for LICs, etc
+     * @return true if launch, false if no launch
+     */
+    public static boolean DECIDE(InputData data) {
+
+        // Note that none of these are implemented.
+        CMV cmv = new CMV(data);
+        PUM pum = new PUM(data.LCM, cmv);
+        FUV fuv = new FUV(pum, data.PUV);
+
+        // If any condition in the final unlocking vector
+        // is false, we don't launch.
+        for (int i = 0; i < fuv.getLength(); i++) {
+            if (!fuv.get(i)) return false;
+            System.out.println("NO");
+        }
+        System.out.println("YES");
+        return true;
+    }
+
 }
