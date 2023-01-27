c3c9aee823353bb0b811e540cfafb31a18fce1c7
estolpe <estolpe@kth.se>
2023-01-27 16:31:53 +0100

    Add skeleton structure for Main

--- src/main/java/org/labs/lab1/Main.java
+++ src/main/java/org/labs/lab1/Main.java
@@ -1,7 +1,17 @@
 package org.labs.lab1;
 
 public class Main {
+
+    /**
+     * @param args
+     * Used to test the LaunchInterceptor with mockup data.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Temporary empty input data, don't use
+        // this constructor for InputData, it's just placeholder.
+        InputData madeUpData = new InputData();
+
+        LaunchInterceptor.DECIDE(madeUpData);
     }
 }
