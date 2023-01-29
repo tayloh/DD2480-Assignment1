2a4976f4bb4243d1c20745ec0715be3fbfa218d7
estolpe <estolpe@kth.se>
2023-01-29 21:08:57 +0100

    Add JUnit depenendy to Maven
    JUnit dependency and updates all .java test files
    with appropriate imports, as well as removes TempTest.java

--- src/test/java/LICTest.java
+++ src/test/java/LICTest.java
@@ -1,2 +1,11 @@
+import org.labs.lab1.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LICTest {
+
+    // Proposed naming convention:
+    // testLIC0_TypeOfData[Positive/Negative/Invalid](...) { }
+
 }
