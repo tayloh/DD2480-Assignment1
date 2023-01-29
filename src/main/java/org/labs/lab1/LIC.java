d69b6f477265c2c3c4a4fe56374d25cd8fbbaf9f
abaz <no.sayers.original@gmail.com>
2023-01-29 18:13:38 +0100

    Add functionality for condition 0 of LIC in (#7)
    Add functionality for condition 0 of LIC in the LIC class, as well as
    test of condition 0 in the LICTest class.

--- src/main/java/org/labs/lab1/LIC.java
+++ src/main/java/org/labs/lab1/LIC.java
@@ -8,9 +8,39 @@ public class LIC {
 
     // Feel free to add helper functions
 
+    /**
+     * Check if condition 0 is met: there exists at least one set of two
+     * consecutive data points that have a distance greater than LENGTH1.
+     *
+     * @param xCoords contains all x coordinates
+     * @param yCoords contains all y coordinates
+     * @param length1 contains LENGTH1 from the input-data
+     * @return if condition 0 is true or not
+     */
     public static boolean condition0(int[] xCoords, int[] yCoords, double length1) {
-        // This is just an example of a LIC declaration
-        return true;
+
+        for(int i = 0 ; i < xCoords.length -1; i++){
+            double distance = calcDistance(xCoords[i],yCoords[i],xCoords[i+1],yCoords[i+1]);
+            if(distance > length1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param xCoord first x coordinate
+     * @param yCoord first y coordinate
+     * @param xCoord1 second x coordinate
+     * @param yCoord1 second y coordinate
+     * @return the distance between the first and second coordinate
+     */
+    private static double calcDistance(int xCoord, int yCoord, int xCoord1, int yCoord1) {
+        double xDiff = xCoord - xCoord1;
+        double yDiff = yCoord - yCoord1;
+
+        return Math.sqrt( Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
     }
 
 }
--- src/test/java/LICTest.java
+++ src/test/java/LICTest.java
@@ -1,2 +1,36 @@
+package test.java;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.labs.lab1.LIC;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LICTest {
+    @Test
+    @DisplayName("Condition 0")
+    void condition0(){
+        int [] x = {1, 2, 3};
+        int [] y = {1, 2, 1};
+
+        assertEquals(true, LIC.condition0(x, y, 1));
+
+        //expected false: distance of 2 is not greater than lenght1 = 2
+        assertEquals(false, LIC.condition0(x, y, 2));
+
+        x = new int[]{-1, 3};
+        y = new int[]{-1, -1};
+
+        assertEquals(true, LIC.condition0(x, y, 3));
+        assertEquals(true, LIC.condition0(x, y, 0));
+
+        //expected false: distance of 4 is not greater than lenght1 = 4
+        assertEquals(false, LIC.condition0(x, y, 4));
+
+        //expected false: there is no distance between two consecutive data points to be greater than lenght1
+        assertEquals(false, LIC.condition0(new int[]{}, new int[]{}, 0));
+
+        //expected false: there is no distance between two consecutive data points to be greater than lenght1
+        assertEquals(false, LIC.condition0(new int[]{2}, new int[]{-3}, 0));
+    }
 }
