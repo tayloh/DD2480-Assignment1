0fb4045549b9e939646e4a7148fde6d2754dacb7
estolpe <estolpe@kth.se>
2023-01-29 20:26:27 +0100

    Add global variable for number of LICs

--- src/main/java/org/labs/lab1/LIC.java
+++ src/main/java/org/labs/lab1/LIC.java
@@ -6,6 +6,13 @@ package org.labs.lab1;
  */
 public class LIC {
 
+
+    /**
+     * The number of launch interceptor conditions.
+     * Defines the sizes of PUV, FUV, etc...
+     */
+    public static int NUM_CONDITIONS = 15;
+
     // Feel free to add helper functions
 
     public static boolean condition0(int[] xCoords, int[] yCoords, double length1) {
