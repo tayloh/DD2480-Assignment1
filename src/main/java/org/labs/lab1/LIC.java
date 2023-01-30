d74c2fa58bfd8e2eae0127ab57a12cfe2cc5291c
estolpe <estolpe@kth.se>
2023-01-30 19:05:56 +0100

    Add LIC condition 4
    Implements LIC condition 4 and adds unit tests
    checking for positive, negative, and invalid

--- src/main/java/org/labs/lab1/LIC.java
+++ src/main/java/org/labs/lab1/LIC.java
@@ -13,4 +13,81 @@ public class LIC {
         return true;
     }
 
+
+    /**
+     * Returns true if there exists at least one set of qPts consecutive data points that
+     * lie in more than quads quadrants, else false.
+     * @param xCoords points x-coordinates
+     * @param yCoords points y-coordinates
+     * @param qPts consecutive data points
+     * @param quads number of quadrants
+     * @param numPoints number of points
+     */
+    public static boolean condition4(double[] xCoords, double yCoords[], int qPts, int quads, int numPoints) {
+        if (!(2 <= qPts && qPts <= numPoints)) {
+            throw new IllegalArgumentException("qPts not within correct range.");
+        }
+
+        if (!(1 <= quads && quads <= 3)) {
+            throw  new IllegalArgumentException("quads not within correct range");
+        }
+
+        for (int i = 0; i < numPoints - qPts; i++) {
+            // For each point, check if the next three points
+            // lie in at least quads+1 different quadrants.
+            // At most, they can lie on 4 different quadrants,
+            // don't just count them; since we need to keep track of which
+            // quadrants has already been "visited":
+            boolean[] quadrantHasPoint = {false, false, false, false};
+
+            for (int conecutiveIndex = 0; conecutiveIndex < qPts; conecutiveIndex++) {
+                double x = xCoords[i + conecutiveIndex];
+                double y = yCoords[i + conecutiveIndex];
+
+                // Priority is given by quadrant number (for when x or y is 0)
+                // Quadrant 1
+                if (x >= 0 && y >= 0) {
+                    quadrantHasPoint[0] = true;
+                }
+                // Quadrant 2
+                else if (x < 0 && y >= 0) {
+                    quadrantHasPoint[1] = true;
+                }
+                // Quadrant 3
+                else if (x <= 0 && y < 0) {
+                    quadrantHasPoint[2] = true;
+                }
+                // Quadrant 4
+                else if (x > 0 && y < 0) {
+                    quadrantHasPoint[3] = true;
+                }
+
+            }
+
+            // Check how many quadrants were visited
+            int consecutiveQuadrants = 0;
+            for (int j = 0; j < quadrantHasPoint.length; j++) {
+                if (quadrantHasPoint[j]) {
+                    consecutiveQuadrants++;
+                }
+            }
+
+            // If it's more than quads, return true
+            if (consecutiveQuadrants > quads) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public static boolean condition5(double[] xCoords, double yCoords[]) {
+        return true;
+    }
+
+    public static boolean condition6(double[] xCoords, double yCoords[]) {
+        return true;
+    }
+
 }
--- src/test/java/LICTest.java
+++ src/test/java/LICTest.java
@@ -8,4 +8,73 @@ public class LICTest {
     // Proposed naming convention:
     // testLIC0_TypeOfData[Positive/Negative/Invalid]() { ... }
 
+    @Test
+    public void testLIC4_Positive() {
+        // Should be true since there is a consecutive sequence of points
+        // that visit the following quadrants in this order:
+        //  ... quadrant 1, quadrant 4, quadrant 2, quadrant 3 ...
+        double[] xCoords = new double[]{0,   0,  1, -1,  0, -1.0};
+        double[] yCoords = new double[]{0.1, 0, -1,  0, -1, -0.5};
+        int qPts = 5;
+        int quads = 3;
+        int numPoints = xCoords.length;
+        boolean result = LIC.condition4(xCoords, yCoords, qPts, quads, numPoints);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void  testLIC4_Negative() {
+        // Should be false since all points lie in the same quadrant
+        // and we request it should be > 1
+
+        double[] xCoords = new double[]{1, 0, 1, 1, 1, 0, 0, 1, 0, 0};
+        double[] yCoords = new double[]{1, 0, 1, 1, 1, 0, 1, 0, 0, 0};
+        int qPts = 10;
+        int quads = 1;
+        int numPoints = xCoords.length;
+
+        boolean result = LIC.condition4(xCoords, yCoords, qPts, quads, numPoints);
+        assertFalse(result);
+
+        // Should be false since qPts is less than quad+1
+        // (we can never visit quad+1 distinct quadrants consecutively using only qPts points then)
+        xCoords = new double[]{0,   0,  1, -1,  0, -1.0};
+        yCoords = new double[]{0.1, 0, -1,  0, -1, -0.5};
+        qPts = 3;
+        quads = 3;
+        numPoints = xCoords.length;
+        result = LIC.condition4(xCoords, yCoords, qPts, quads, numPoints);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testLIC4_Invalid() {
+        // Invalid since qPts=1 is less than 2
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> LIC.condition4(null, null, 1, 3, 10)
+        );
+
+        // Invalid since qPts=11 is more than numPoints=10
+        // This could be implemented to return false instead I guess
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> LIC.condition4(null, null, 11, 3, 10)
+        );
+
+        // Invalid since quads=0 is less than 1
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> LIC.condition4(null, null, 5, 0, 10)
+        );
+
+        // Invalid since quads=4 is larger than 3
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> LIC.condition4(null, null, 5, 4, 10)
+        );
+    }
+
 }
