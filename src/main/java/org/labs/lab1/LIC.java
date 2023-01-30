6a455316e59ba57f5acccfa858496bc1f167a805
estolpe <estolpe@kth.se>
2023-01-30 20:58:22 +0100

    Add implementation of LIC 6

--- src/main/java/org/labs/lab1/LIC.java
+++ src/main/java/org/labs/lab1/LIC.java
@@ -1,5 +1,6 @@
 package org.labs.lab1;
 
+
 /**
  * Utility class containing functions
  * for calculating Launch Interceptor Conditions (LICs) 0-14.
@@ -104,8 +105,85 @@ public class LIC {
         return false;
     }
 
-    public static boolean condition6(double[] xCoords, double yCoords[]) {
-        return true;
+    /**
+     * Returns true if there exists at least one set of nPts consecutive data points
+     * such that at least one of the points lies a distance greater than dist from the line
+     * joining the first and the last of these nPts points. If the first and last points of
+     * these nPts are identical, then the calculated distance to compare with dist will be
+     * the distance from the coincident point to all other points of the nPts consecutive points.
+     * The condition is not met when numPoints < 3. Else, returns false.
+     * @param xCoords points x-coordinates
+     * @param yCoords points y-coordinates
+     * @param nPts consecutive data points
+     * @param dist distance to compare with
+     * @param numPoints number of points
+     */
+    public static boolean condition6(double[] xCoords, double yCoords[], int nPts, double dist, int numPoints) {
+        if (numPoints < 3) {
+            return false;
+        }
+
+        if (!(3 <= nPts && nPts <= numPoints)) {
+            throw new IllegalArgumentException("nPts not within correct range.");
+        }
+
+        if (!(0 <= dist)) {
+            throw new IllegalArgumentException("dist not within correct range.");
+        }
+
+        // <= since we do -1 in if statement below we need one extra iteration
+        // to reach the final element
+        // note that i + nPts <= numPoints so always -1 in that case, or we go out of bounds
+        for (int i = 0; i <= numPoints - nPts; i++) {
+            double firstPointX = xCoords[i];
+            double firstPointY = yCoords[i];
+
+            // -1 since otherwise we consider nPts+1 points
+            double lastPointX = xCoords[i + nPts - 1];
+            double lastPointY = yCoords[i + nPts - 1];
+
+            // If fist and last consecutive points coincide
+            if (firstPointX == lastPointX && firstPointY == lastPointY) {
+                double compareX = xCoords[i];
+                double compareY = yCoords[i];
+
+                // Check if any of the consec. points is larger than dist away from the coinciding point
+                // Skip first and last since they're the ones coinciding
+                for (int j = 1; j < nPts - 1; j++) {
+                    double currX = xCoords[i+j];
+                    double currY = yCoords[i+j];
+
+                    // Current points is further away than dist
+                    if (Math.hypot(compareX - currX, compareY - currY) > dist) {
+                        return true;
+                    }
+                }
+            }
+            // If they don't coincide
+            else {
+                // Again, skip the endpoints of the consecutive points
+                for (int j = 1; j < nPts - 1; j++) {
+                    double currX = xCoords[i+j];
+                    double currY = yCoords[i+j];
+
+                    // Formula from: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+                    double denominator =
+                            (lastPointX - firstPointX) * (firstPointY - currY) -
+                            (firstPointX - currX) * (lastPointY - firstPointY);
+
+                    double numerator =
+                            Math.pow(lastPointX - firstPointX, 2) + Math.pow(lastPointY - firstPointY, 2);
+
+                    double distanceToLine = Math.abs(denominator) / Math.sqrt(numerator);
+
+                    if (distanceToLine > dist) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
 }
