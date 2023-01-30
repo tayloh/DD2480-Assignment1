50619b0444611a372060a82af67f90ef6c31ee98
Hannes Hallqvist <hannes.hallqvist@hotmail.com>
2023-01-30 21:39:50 +0100

    Extract find point triplet into separate function

--- src/main/java/org/labs/lab1/LIC.java
+++ src/main/java/org/labs/lab1/LIC.java
@@ -29,6 +29,30 @@ public class LIC {
     }
 
     /**
+     * Finds a point triplet from a given respective point distance between them
+     * 
+     * @param xCoordinates an array of x-coordinates
+     * @param yCoordinates an array of y-coordinates
+     * @param offset       the offset from where the first point is checked
+     * @param distanceA    the distance between the first and the second point
+     * @param distanceB    the distance between the second and third point
+     * @return The three found points
+     */
+    public static Point2D.Double[] findPointTriplet(double[] xCoordinates, double[] yCoordinates, int offset,
+            int distanceA, int distanceB) {
+
+        int firstIndex = offset;
+        int secondIndex = offset + distanceA + 1;
+        int thirdIndex = offset + distanceA + distanceB + 2;
+
+        var first = new Point2D.Double(xCoordinates[firstIndex], yCoordinates[firstIndex]);
+        var second = new Point2D.Double(xCoordinates[secondIndex], yCoordinates[secondIndex]);
+        var third = new Point2D.Double(xCoordinates[thirdIndex], yCoordinates[thirdIndex]);
+
+        return new Point2D.Double[] { first, second, third };
+    }
+
+    /**
      * Checks the condition 9 of the LIC.
      * Asserts that there is a triplet of points where the angle between them is
      * more than PI + epsilon or less than PI - epsilon.
@@ -51,24 +75,24 @@ public class LIC {
         int nPossibleTriplets = numPoints - (cPts + dPts + 2);
         for (int i = 0; i < nPossibleTriplets; i++) {
             // extract points
-            int firstIndex = i;
-            int secondIndex = i + cPts + 1;
-            int thirdIndex = i + cPts + dPts + 2;
-
-            var first = new Point2D.Double(xCoordinates[firstIndex], yCoordinates[firstIndex]);
-            var second = new Point2D.Double(xCoordinates[secondIndex], yCoordinates[secondIndex]);
-            var third = new Point2D.Double(xCoordinates[thirdIndex], yCoordinates[thirdIndex]);
+            var points = findPointTriplet(xCoordinates, yCoordinates, i, cPts, dPts);
 
             // if angle is invalid, skip triplet
-            if (first.equals(second) || second.equals(third)) {
+            if (points[0].equals(points[1]) || points[1].equals(points[2])) {
                 continue;
             }
             // check angle requirement
-            double angle = calculateAngle(first, second, third);
+            double angle = calculateAngle(points[0], points[1], points[2]);
             if (angle < (Math.PI - epsilon) || angle > (Math.PI + epsilon)) {
                 return true;
             }
         }
+        return false;
+    }
+
+    public static boolean condition10(double[] xCoordinates, double[] yCoordinates, int ePts, int fPts, double area1,
+            int numPoints) {
+
         return false;
     }
 
