a035d5c0239738208ffc5e93c664cf4c0ba764cd
Tobias Mattsson <tmatts@kth.se>
2023-01-31 19:52:35 +0100

    Add CMV LIC calculations in constructor (#8)

--- src/main/java/org/labs/lab1/CMV.java
+++ src/main/java/org/labs/lab1/CMV.java
@@ -10,6 +10,22 @@ public class CMV {
     // TODO doc
     public CMV(InputData data) {
         // Computes cmv using LIC static methods
+        cmv = new boolean[15];
+        cmv[0] = LIC.condition0(data.xCoordinates, data.yCoordinates, data.length1);
+        cmv[1] = LIC.condition1(data.xCoordinates, data.yCoordinates, data.radius1);
+        cmv[2] = LIC.condition2(data.xCoordinates, data.yCoordinates, data.epsilon);
+        cmv[3] = LIC.condition3(data.xCoordinates, data.yCoordinates, data.area1);
+        cmv[4] = LIC.condition4(data.xCoordinates, data.yCoordinates, data.qPts, data.quads, data.numPoints);
+        cmv[5] = LIC.condition5(data.xCoordinates, data.yCoordinates);
+        cmv[6] = LIC.condition6(data.xCoordinates, data.yCoordinates, data.nPts, data.dist, data.numPoints);
+        cmv[7] = LIC.condition7(data.xCoordinates, data.yCoordinates, data.kPts, data.length1, data.numPoints);
+        cmv[8] = LIC.condition8(data.xCoordinates, data.yCoordinates, data.aPts, data.bPts, data.radius1, data.numPoints);
+        cmv[9] = LIC.condition9(data.xCoordinates, data.yCoordinates, data.cPts, data.dPts, data.epsilon, data.numPoints);
+        cmv[10] = LIC.condition10(data.xCoordinates, data.yCoordinates, data.ePts, data.fPts, data.area1, data.numPoints);
+        cmv[11] = LIC.condition11(data.xCoordinates, data.yCoordinates, data.gPts, data.numPoints);
+        cmv[12] = LIC.condition12(data.xCoordinates, data.yCoordinates, data.kPts, data.length1, data.length2, data.numPoints);
+        cmv[13] = LIC.condition13(data.xCoordinates, data.yCoordinates, data.aPts, data.bPts, data.radius1, data.radius2, data.numPoints);
+        cmv[14] = LIC.condition14(data.xCoordinates, data.yCoordinates, data.ePts, data.fPts, data.area1, data.area2, data.numPoints);
     }
 
     /**
